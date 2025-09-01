8766642362ad6427e1da3a3af64aa928790018e0
z3lx <57017122+z3lx@users.noreply.github.com>
2025-09-01 15:31:03 -0400

    Implement cause exception chaining in TracedException

--- include/pwu/TracedException.hpp
+++ include/pwu/TracedException.hpp
@@ -2,42 +2,61 @@
 
 #include <exception>
 #include <source_location>
+#include <span>
 #include <string>
+#include <string_view>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 namespace pwu {
 class TracedException final : public std::exception {
 public:
-    explicit TracedException(
-        std::exception_ptr exception,
-        std::source_location location = std::source_location::current()
-    );
-
-    explicit TracedException(
-        TracedException& exception,
-        std::source_location location = std::source_location::current()
-    );
-
-    explicit TracedException(
-        TracedException&& exception,
-        std::source_location location = std::source_location::current()
+    TracedException(
+        std::exception_ptr previous,
+        std::exception_ptr current,
+        std::source_location location
     );
 
     ~TracedException() noexcept override;
 
-    [[nodiscard]] std::exception_ptr
-    GetOriginalException() const noexcept;
-    [[nodiscard]] const std::vector<std::source_location>&
-    GetTrace() const noexcept;
+    [[nodiscard]] std::exception_ptr GetOriginal() const noexcept;
+    [[nodiscard]] std::exception_ptr GetCause() const noexcept;
+    [[nodiscard]] std::span<const std::source_location> GetTrace() const noexcept;
+    [[nodiscard]] std::string_view GetFormattedTrace() const noexcept;
+
+    void ThrowOriginal() const;
+    void ThrowCause() const;
 
     [[nodiscard]] const char* what() const noexcept override;
 
 private:
-    void AppendTrace(std::exception_ptr exception);
-    void AppendTrace(std::source_location location);
-
-    std::exception_ptr originalException;
-    std::string formattedTrace;
+    std::exception_ptr original;
+    std::exception_ptr cause;
     std::vector<std::source_location> trace;
+    std::string message;
 };
+
+template <typename Callable> requires std::is_invocable_v<Callable>
+void CatchThrowTraced(
+    Callable&& callable,
+    const std::source_location location = std::source_location::current()) {
+    const std::exception_ptr previous = std::current_exception();
+    try {
+        std::forward<Callable>(callable)();
+    } catch (...) {
+        const std::exception_ptr current = std::current_exception();
+        throw TracedException { previous, current, location };
+    }
+}
+
+inline void ThrowCaughtTraced(
+    const std::source_location location = std::source_location::current()) {
+    const std::exception_ptr exception = std::current_exception();
+    try {
+        std::rethrow_exception(exception);
+    } catch (...) {
+        throw TracedException { nullptr, exception, location };
+    }
+}
 } // namespace pwu
--- src/TracedException.cpp
+++ src/TracedException.cpp
@@ -2,80 +2,141 @@
 
 #include <exception>
 #include <format>
+#include <iterator>
 #include <source_location>
+#include <span>
+#include <string>
+#include <string_view>
 #include <thread>
 #include <typeinfo>
-#include <utility>
-#include <vector>
+
+namespace {
+void AppendOriginal(std::string& message, const std::exception* original) {
+    if (original != nullptr){
+        std::format_to(
+            std::back_inserter(message),
+            "Exception in thread {} {}: {}",
+            std::this_thread::get_id(),
+            typeid(*original).name(),
+            original->what()
+        );
+    } else {
+        std::format_to(
+            std::back_inserter(message),
+            "Exception in thread {}: Unknown exception",
+            std::this_thread::get_id()
+        );
+    }
+}
+
+void AppendCause(std::string& message, const std::exception* cause) {
+    if (cause != nullptr) {
+        std::format_to(
+            std::back_inserter(message),
+            "\nCaused by {}: {}",
+            typeid(*cause).name(),
+            cause->what()
+        );
+    } else {
+        std::format_to(
+            std::back_inserter(message),
+            "\nCaused by: Unknown exception"
+        );
+    }
+}
+
+void AppendTrace(
+    std::string& message,
+    const std::span<const std::source_location> trace) {
+    for (const std::source_location traceElement : trace) {
+        std::format_to(
+            std::back_inserter(message),
+            "\n    at {} ({}:{}:{})",
+            traceElement.function_name(),
+            traceElement.file_name(),
+            traceElement.line(),
+            traceElement.column()
+        );
+    }
+}
+} // namespace
 
 namespace pwu {
 TracedException::TracedException(
-    const std::exception_ptr exception,
-    const std::source_location location)
-    : originalException { exception } {
-    AppendTrace(exception);
-    AppendTrace(location);
-}
+    const std::exception_ptr previous,
+    const std::exception_ptr current,
+    const std::source_location location) {
+    // Set original, cause, and trace
+    try {
+        std::rethrow_exception(current);
+    } catch (const TracedException& exception) {
+        original = exception.original;
+        trace.append_range(exception.trace);
+        cause = exception.cause;
+    } catch (...) {
+        original = current;
+        if (previous != nullptr) try {
+            std::rethrow_exception(previous);
+        } catch (const TracedException&) {
+            cause = previous;
+        } catch (...) {}
+    }
+    trace.push_back(location);
 
-TracedException::TracedException(
-    TracedException& exception,
-    std::source_location location)
-    : originalException { exception.originalException }
-    , formattedTrace { exception.formattedTrace }
-    , trace { exception.trace } {
-    AppendTrace(location);
-}
-
-TracedException::TracedException(
-    TracedException&& exception,
-    const std::source_location location)
-    : originalException { std::move(exception.originalException) }
-    , formattedTrace { std::move(exception.formattedTrace) }
-    , trace { std::move(exception.trace) } {
-    AppendTrace(location);
+    // Format message
+    try {
+        std::rethrow_exception(original);
+    } catch (const std::exception& exception) {
+        AppendOriginal(message, &exception);
+    } catch (...) {
+        AppendOriginal(message, nullptr);
+    }
+    AppendTrace(message, trace);
+    std::exception_ptr nextCause = cause;
+    while (nextCause != nullptr) {
+        try {
+            std::rethrow_exception(nextCause);
+        } catch (const TracedException& exception) {
+            nextCause = exception.cause;
+            try {
+                std::rethrow_exception(exception.original);
+            } catch (const std::exception& exception) {
+                AppendCause(message, &exception);
+            } catch (...) {
+                AppendCause(message, nullptr);
+            }
+            AppendTrace(message, exception.trace);
+        }
+    }
 }
 
 TracedException::~TracedException() noexcept = default;
 
-std::exception_ptr
-TracedException::GetOriginalException() const noexcept {
-    return originalException;
+std::exception_ptr TracedException::GetOriginal() const noexcept {
+    return original;
 }
 
-const std::vector<std::source_location>&
-TracedException::GetTrace() const noexcept {
+std::exception_ptr TracedException::GetCause() const noexcept {
+    return cause;
+}
+
+std::span<const std::source_location> TracedException::GetTrace() const noexcept {
     return trace;
 }
 
+std::string_view TracedException::GetFormattedTrace() const noexcept {
+    return message;
+}
+
+void TracedException::ThrowOriginal() const {
+    std::rethrow_exception(original);
+}
+
+void TracedException::ThrowCause() const {
+    std::rethrow_exception(cause);
+}
+
 const char* TracedException::what() const noexcept {
-    return formattedTrace.c_str();
-}
-
-void TracedException::AppendTrace(const std::exception_ptr exception) try {
-    std::rethrow_exception(exception);
-} catch (const std::exception& e) {
-    formattedTrace.append(std::format(
-        "Exception in thread {} {}: {}",
-        std::this_thread::get_id(),
-        typeid(e).name(),
-        e.what()
-    ));
-} catch (...) {
-    formattedTrace.append("Unknown exception");
-}
-
-void TracedException::AppendTrace(const std::source_location location) {
-    if (location.file_name() == nullptr ||
-        location.file_name()[0] == '\0') {
-        return;
-    }
-    trace.push_back(location);
-    formattedTrace.append(std::format(
-        "\n    at {} ({}:{}:{})",
-        location.function_name(),
-        location.file_name(),
-        location.line(),
-        location.column()
-    ));
+    return message.c_str();
 }
 } // namespace pwu
